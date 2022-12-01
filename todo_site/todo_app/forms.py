7f081d573d47e052e22fa53969baa0bf93b201af
Christy Hayter <christyhayter@Christys-MBP.lan>
2022-12-01 17:11:16 -0500

    todosite

--- todo_site/todo_app/forms.py
+++ todo_site/todo_app/forms.py
@@ -1,9 +1,27 @@
 from django.forms import ModelForm
 
-from todo_app.models import Task
+from todo_app.models import Task, Comment
 
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ['description']
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['body']
+
+
+    def __init__(self, *args, **kwargs):
+        task = kwargs.pop('task_object')
+        super().__init__(*args, **kwargs)
+
+        #self.instance is the comment we are creating with this form
+        self.instance.task = task
+
+
+
+
+        #the form will get created with a task, we need to pull that task out and keep track of it
--- todo_site/todo_app/views.py
+++ todo_site/todo_app/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import redirect, render
 
 from django.views import View
 
-from todo_app.forms import TaskForm
+from todo_app.forms import TaskForm, CommentForm
 
-from todo_app.models import Task
+from todo_app.models import Task, Comment
 # Create your views here.
 
 
@@ -33,3 +33,38 @@ class HomeView(View):
 class TaskDetailView(View):
     def get(self, request, task_id):
         task = Task.objects.get(id=task_id)
+        
+        task_form = TaskForm(instance=task)
+
+        comments = Comment.objects.filter(task=task)
+        comment_form = CommentForm(task_object=task)
+
+        html_data = {
+            'task_object': task,
+            'form': task_form,
+            'comment_list': comments,
+            'comment_form': comment_form,
+            
+        }
+
+        return render(
+            request=request,
+            template_name='detail.html',
+            context=html_data,
+        )
+
+    def post(self, request, task_id):
+        task = Task.objects.get(id=task_id)
+
+        if 'update' in request.POST:
+            task_form= TaskForm(request.POST, instance=task)
+            task_form.save()
+        elif 'delete' in request.POST:
+            task.delete()
+        elif 'add' in request.POST:
+            comment_form = CommentForm(request.POST, task_object=task)
+            comment_form.save()
+            return redirect('task_detail', task.id)
+        
+
+        return redirect('home')
